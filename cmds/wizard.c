8080546b2bd571c89754c1fbdf023a55eb5cf584
Oiuv <i@oiuv.cn>
2023-04-04 14:36:38 +0800

    更改管理员IP为配置文件

--- cmds/wizard.c
+++ cmds/wizard.c
@@ -2,7 +2,7 @@ int main(object me, string arg)
 {
     function f;
 
-    if (member_array(query_ip_number(me), ({"127.0.0.1", "118.190.104.241", "47.88.55.34"})) < 0)
+    if (member_array(query_ip_number(me), json_decode(read_file("config.json"))["wizard_ip"]) < 0)
     {
         return 0;
     }
