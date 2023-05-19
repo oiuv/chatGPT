818b9c492147bbade4a20d8973647b84431835d6
Oiuv <i@oiuv.cn>
2023-05-19 13:37:54 +0800

    增加多个密钥的余额查询

--- cmds/credit_grants.c
+++ cmds/credit_grants.c
@@ -4,14 +4,24 @@ inherit _EXTERNAL_CMD;
 int main(object me, string arg)
 {
     int CURL_CMD = 1;
-    string key = json_decode(read_file("config.json"))["session_key"];
+    string *keys = json_decode(read_file("config.json"))["session_keys"];
     string proxy = json_decode(read_file("config.json"))["proxy"];
-    string *cURL = ({"-s", "https://api.openai.com/dashboard/billing/credit_grants", "--header", "Authorization: Bearer " + key});
+    string key;
+    string *cURL = ({"-s", "https://api.openai.com/dashboard/billing/credit_grants", "--header"});
+
+    if (to_int(arg) < 1 || to_int(arg) > sizeof(keys))
+        key = element_of(keys);
+    else
+        key = keys[to_int(arg) - 1];
+
+    cURL += ({"Authorization: Bearer " + key});
+
     if (__ARCH__ == "Microsoft Windows")
         CURL_CMD = 2;
     if (proxy)
         cURL += ({"-x", proxy});
 
+    // debug(cURL);
     external_cmd(CURL_CMD, cURL);
 
     return 1;
@@ -21,10 +31,17 @@ protected void response(string result)
 {
     mapping data = json_decode(result);
     string arg;
+    // debug(result);
     if (data["error"])
         arg = sprintf("%serror：%s\n", HIG "【chatGPT】" NOR, data["error"]["message"]);
+    else if (data["grants"])
+    {
+        mapping grants = data["grants"]["data"][0];
+        arg = sprintf("%O", grants);
+        arg = sprintf("%s截止 %s 额度信息：\nid : %s\n账户总额 : $%.2f\n消费额度 : $%.2f\n账户余额 : $%.2f\n生效时间 : %s\n过期时间 : %s\n", HIG "【chatGPT】" NOR, ctime(), grants["id"], data["total_granted"], data["total_used"], data["total_available"], ctime(to_int(grants["effective_at"])), ctime(to_int(grants["expires_at"])));
+    }
     else
-        arg = sprintf("%sAPI额度：total_used : $%.2f, total_available : $%.2f\n", HIG "【chatGPT】" NOR, data["total_used"], data["total_available"]);
+        arg = result;
 
     shout(arg);
 }
